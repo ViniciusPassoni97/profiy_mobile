10bd6dba5d0c94fe36eb6bae28402817b69a2c88
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-11 22:01:20 -0300

    Adicionando googel fonts

--- src/pages/landing/style.ts
+++ src/pages/landing/style.ts
@@ -12,13 +12,14 @@ const LandingStyle = StyleSheet.create({
         resizeMode:'contain'
     },
     title: {
+        fontFamily:'Poppins_400Regular',
         color:'#fff',
         lineHeight:30,
         marginTop:80,
         fontSize:20
     },
     titleBold: {
-        fontWeight:'bold'
+        fontFamily:'Poppins_600SemiBold',
     }
 });
 
