dc4aece256869b44e35b609252ddbf0aa0c9925f
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-17 21:56:08 -0300

    Goback em src/pages/giveClasses

--- src/pages/giveClasses/index.tsx
+++ src/pages/giveClasses/index.tsx
@@ -7,8 +7,14 @@ import {
 import GiveClassStyle from './style';
 import Background from '../../assets/img/background.png'
 import { RectButton } from 'react-native-gesture-handler';
+import {useNavigation} from'@react-navigation/native';
 
 export default function GiveClass() {
+    const {goBack} = useNavigation();
+
+    function handleClick() {
+        goBack();
+    }
     return(
         <View style={GiveClassStyle.container}>
             <ImageBackground resizeMode='contain' source={Background} style={GiveClassStyle.containerBackground}> 
@@ -18,7 +24,7 @@ export default function GiveClass() {
                 na nossa plataforma web.
                 </Text>
             </ImageBackground>
-            <RectButton style={GiveClassStyle.okButton}>
+            <RectButton onPress={handleClick} style={GiveClassStyle.okButton}>
                 <Text style={GiveClassStyle.okButtonText}>Tudo Bem</Text>
             </RectButton>
         </View>
