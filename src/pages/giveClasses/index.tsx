42eb95ddadf26d761b5a7d649af2ec5ddd7d57e7
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-17 21:51:34 -0300

    Styles em src/pages/giveClasses

--- src/pages/giveClasses/index.tsx
+++ src/pages/giveClasses/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    ImageBackground
 } from 'react-native';
+import GiveClassStyle from './style';
+import Background from '../../assets/img/background.png'
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function GiveClass() {
     return(
-        <View>
-            <Text>Profy</Text>
+        <View style={GiveClassStyle.container}>
+            <ImageBackground resizeMode='contain' source={Background} style={GiveClassStyle.containerBackground}> 
+                <Text style={GiveClassStyle.title}>Quer ser um Proffy?</Text>
+                <Text style={GiveClassStyle.description}>Para começar, você precisa
+                se cadastrar como professor
+                na nossa plataforma web.
+                </Text>
+            </ImageBackground>
+            <RectButton style={GiveClassStyle.okButton}>
+                <Text style={GiveClassStyle.okButtonText}>Tudo Bem</Text>
+            </RectButton>
         </View>
     );
 }
